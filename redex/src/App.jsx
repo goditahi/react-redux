827388146c67d9b738ba57dbe2961d6b1504af16
goditahi <131813354+goditahi@users.noreply.github.com>
2024-08-11 14:11:51 +0000

    todos

--- redex/src/App.jsx
+++ redex/src/App.jsx
@@ -1,7 +1,16 @@
-export default function App() {
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
   return (
-    <h1 className="text-3xl text-center text-green-200 font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
   )
 }
+
+export default App
